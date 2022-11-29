7b81f586374013c1918a09057d60dd0227ea1f1f
biosphere <dev.biosphere@gmail.com>
2022-11-29 20:43:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Link, Routes } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {
+  Homepage,
+  News,
+  Cryptocurrencies,
+  CryptoDetails,
+  Navbar,
+} from "./components";
+import "./App.css";
+
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
     </div>
-  );
-}
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+            <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+            <Route path="/news" element={<News />} />
+          </Routes>
+        </div>
+      </Layout>
+      <div className="footer">
+        <Typography.Title
+          level={5}
+          style={{ color: "white", textAlign: "center" }}
+        >
+          Copyright © 2021
+          <Link to="/">Cryptoverse Inc.</Link> <br />
+          All Rights Reserved.
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/news">News</Link>
+        </Space>
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
---
+++ src/components/notFound.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const notFound = () => {
+  return <div>404 Page Not Found !!!</div>;
+};
+
+export default notFound;
